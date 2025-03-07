f8302361b8474c7f67e776216a297f1a2ba2e8b6
Franco Canzani <francocanzani@gmail.com>
2025-03-07 22:26:13 +0100

    chore: Refactor EditorFooter component for improved readability and responsiveness

--- src/components/editor-footer.tsx
+++ src/components/editor-footer.tsx
@@ -1,12 +1,21 @@
 import { Editor } from '@tiptap/core';
 
-export default function EditorFooter({ editor, isSaved }: { editor: Editor, isSaved: boolean }) {
+export default function EditorFooter({
+  editor,
+  isSaved,
+}: {
+  editor: Editor;
+  isSaved: boolean;
+}) {
   return (
-    <div className='flex items-center p-3 justify-between w-full text-xs text-gray-600 space-x-2'>
-     {isSaved && <span>Saved</span>}
-     <div className='flex items-center justify-end w-full'> <p>{editor.storage.characterCount.characters()} characters</p>
-      <span>/</span>
-      <p>{editor.storage.characterCount.words()} words</p></div>
+    <div className='flex items-center p-3 justify-between max-w-4xl mx-auto w-full text-xs text-gray-600 space-x-2'>
+      {isSaved && <span>Saved</span>}
+      <div className='flex items-center justify-end w-full'>
+        {' '}
+        <p>{editor.storage.characterCount.characters()} characters</p>
+        <span>/</span>
+        <p>{editor.storage.characterCount.words()} words</p>
+      </div>
     </div>
   );
 }
