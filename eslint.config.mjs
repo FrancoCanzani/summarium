55482f74688310af3698ea7e232899f21ebea526
Franco Canzani <francocanzani@gmail.com>
2025-03-27 21:47:06 +0100

    Comment out eslint rules for now

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,8 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 import { FlatCompat } from "@eslint/eslintrc";
-import eslintPluginImport from 'eslint-plugin-import'; // Import the plugin
-import eslintPluginReact from 'eslint-plugin-react'; // Import the plugin
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintPluginReact from 'eslint-plugin-react';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -19,20 +19,20 @@ const eslintConfig = [
       "import": eslintPluginImport, // Use the imported plugin instance
       "react": eslintPluginReact, // Use the imported plugin instance
     },
-    rules: {
-      // General JavaScript rules
-      "no-console": ["warn", { allow: ["warn", "error"] }], // Warn for console.log, allow console.warn and console.error
-      "prefer-const": "error", // Enforce const for variables that are never reassigned
-      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }], // Ignore unused variables starting with _
-      "max-lines": [
-        "warn",
-        { max: 300, skipBlankLines: true, skipComments: true },
-      ], // Warn if a file exceeds 300 lines
-      "no-duplicate-imports": "error", // Disallow duplicate imports
+    // rules: {
+    //   // General JavaScript rules
+    //   "no-console": ["warn", { allow: ["warn", "error"] }], // Warn for console.log, allow console.warn and console.error
+    //   "prefer-const": "error", // Enforce const for variables that are never reassigned
+    //   "no-unused-vars": ["error", { argsIgnorePattern: "^_" }], // Ignore unused variables starting with _
+    //   "max-lines": [
+    //     "warn",
+    //     { max: 300, skipBlankLines: true, skipComments: true },
+    //   ], // Warn if a file exceeds 300 lines
+    //   "no-duplicate-imports": "error", // Disallow duplicate imports
 
-      // React-specific rules
-      "react/no-array-index-key": "warn", // Warn against using array indices as keys in React lists
-    },
+    //   // React-specific rules
+    //   "react/no-array-index-key": "warn", // Warn against using array indices as keys in React lists
+    // },
   },
 ];
 
