2ca2267ec4bcc2f89a4edafd2012e0a9fda609ec
Franco Canzani <francocanzani@gmail.com>
2025-04-03 19:14:54 +0200

    Use environment variable for API endpoint URL

--- src/lib/fetchers.ts
+++ src/lib/fetchers.ts
@@ -1,12 +1,15 @@
 import { Note } from "./types";
 
 export const fetchNote = async (id: string): Promise<Note> => {
-  const response = await fetch(`http://localhost:3000/api/note/${id}`, {
-    cache: "force-cache",
-    next: {
-      tags: [`note-${id}`],
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_URL}/api/note/${id}`,
+    {
+      cache: "force-cache",
+      next: {
+        tags: [`note-${id}`],
+      },
     },
-  });
+  );
   if (!response.ok) {
     throw new Error("Failed to fetch note");
   }
