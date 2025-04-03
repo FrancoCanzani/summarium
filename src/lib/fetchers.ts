bf01433a8c8a05574721e07516f4bc5e3c258327
Franco Canzani <francocanzani@gmail.com>
2025-04-03 19:46:07 +0200

    Remove caching config from note fetch request

--- src/lib/fetchers.ts
+++ src/lib/fetchers.ts
@@ -3,12 +3,7 @@ import { Note } from "./types";
 export const fetchNote = async (id: string): Promise<Note> => {
   const baseUrl = process.env.NEXT_PUBLIC_URL || "";
 
-  const response = await fetch(`${baseUrl}/api/note/${id}`, {
-    cache: "force-cache",
-    next: {
-      tags: [`note-${id}`],
-    },
-  });
+  const response = await fetch(`${baseUrl}/api/note/${id}`);
 
   if (!response.ok) {
     throw new Error("Failed to fetch note");
