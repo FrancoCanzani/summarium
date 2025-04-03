eb3716872f66f2349605a753467e2365a504d4f6
Franco Canzani <francocanzani@gmail.com>
2025-04-03 12:56:31 +0200

    fix

--- src/app/(dashboard)/notes/[id]/page.tsx
+++ src/app/(dashboard)/notes/[id]/page.tsx
@@ -18,7 +18,7 @@ export default async function EditorPage({
   const id = result.data;
   
   try {
-    const headersList = new Headers(headers())
+    const headersList = new Headers(await headers())
     const host = headersList.get("host") || "";
     const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
     
